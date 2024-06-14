4eb8b873f9351c45edd4dc3e347474ae102bda77
yakki12345 <15801371926@163.com>
2024-06-14 21:41:43 +0800

    the res

---
+++ usep.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 521;
+    int b = 125;
+    int c = a + b;
+    cout << "Standard exception: " <<c<<endl;
+}
+
